32fa7b183c6f118a58baecf85734083a041aa5b9
twosoull <71281491+twosoull@users.noreply.github.com>
2020-12-02 15:57:13 +0900

    조금있다 리뷰

--- src/com/javaex/ex06/MyBase.java
+++ src/com/javaex/ex06/MyBase.java
@@ -2,6 +2,19 @@ package com.javaex.ex06;
 
 public class MyBase extends Base{
 
-    //코드작성
-    
+    public void service(String state) {
+    	if(state.equals("오후")) {
+    		afternoon();
+    		
+    	}else {
+    		super.service(state);
+    	}
+    	
+    }
+    public void day() {
+    	System.out.println("낮에는 열심히 수업듣자");
+    }
+    public void afternoon() {
+    	System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+    }
 }
--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -3,12 +3,22 @@ package com.javaex.ex07;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
-        
-        //코드작성
+        if("앱".equals(str)) {
+        	app();
+        }else  {
+        	super.execute(str);
+        }
         
     }
  
     //메소드작성
+    public void app() {
+    	System.out.println("앱실행");
+    }
+    
+    public void playMusic() {
+    	System.out.println("다운로드해서 음악재생");
+    }
     
     //메소드작성
     
